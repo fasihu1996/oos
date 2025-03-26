88787a7cf12c5388d9e803e274ea1f447af78c8a
JonEffingSnow <fasih.uddin@empiriecom.com>
2025-03-26 15:33:32 +0100

    generator adjusted, comments added

--- afg4_generator.py
+++ afg4_generator.py
@@ -1,13 +1,11 @@
 def small_words(eingabe):
     for e in eingabe:
         if len(e) < 3:
-            yield e
-        else:
-            yield None
+            yield e # das ist schon eine generator funktion
     # g = (yield e for e in eingabe if e.len < 3)
 
 if __name__ == '__main__':
     words = ['a', 'ab', 'abc', 'abcd', 'abcde']
-    res = (word for word in small_words(words))
-    for r in res:
+    # res = (word for word in small_words(words)) ## Generator expression, nicht notwendig
+    for r in small_words(words): #
         print(r)
